c521f4e17f18b2150a30a80cc4da2d21221b88c3
phibersoft <spirnet@hotmail.com>
2022-10-27 02:53:50 +0300

    Shortcut Size

--- SVG/LibraryIcon.tsx
+++ SVG/LibraryIcon.tsx
@@ -13,12 +13,14 @@ const LibraryIcon: FC = () => {
       <path
         d="M12.75,3H5.25A1.15,1.15,0,0,0,4,4V33H14V4A1.15,1.15,0,0,0,12.75,3Z"
         className="clr-i-solid clr-i-solid-path-1"
+        fill="#65350F"
       ></path>
       <path
         d="M33.77,31.09l-6.94-18.3a1,1,0,0,0-1.29-.58L22,13.59V9a1,1,0,0,0-1-1H16V33h6V14.69L28.93,33Z"
         className="clr-i-solid clr-i-solid-path-2"
+        fill={"#707070"}
       ></path>
-      <rect x="0" y="0" width="36" height="36" fillOpacity="0" />
+      <rect x="0" y="0" width="36" height="36" fillOpacity="0" fill={"#bfff00"} />
     </svg>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,21 @@
 import type { NextPage } from "next";
 import { Desktop } from "../components/Desktop";
+import {useEffect, useState} from 'react';
 
 const Home: NextPage = () => {
+  const [configured, setConfigured] = useState(false);
+
+  useEffect(() => {
+    const { outerWidth } = window;
+
+    if(outerWidth <= 500){
+      document.documentElement.style.setProperty('--shortcut-size', '56px');
+    }
+
+    setConfigured(true);
+  }, []);
+
+  if(!configured) return null;
   return <Desktop />;
 };
 
